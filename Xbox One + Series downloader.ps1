045310f3ec12a9767cf9140038d6cb9dc307c2f3
Randy2727 <19766404+Randy2727@users.noreply.github.com>
2023-06-12 21:13:30 -0700

    Create Xbox One + Series downloader.ps1

---
+++ Xbox One + Series downloader.ps1
@@ -0,0 +1,14 @@
+﻿#Prompt user to enter URL to download
+$URL = Read-Host "Enter the CDN URL of the game you would like to download"
+
+#Gets the ContentID from the URL and converts string to upper case. Stores value as $NameUpper
+$NameUpper = ($URL -split "/")[5].toupper()
+
+#Downloads the file based on user entered URL, outputs file to the location of script, and names files as the uppercase ContentID for console use.
+$Priority = "Normal"
+Start-BitsTransfer -Source $URL -Destination $NameUpper -Priority $Priority
+
+Read-Host "Your file has been downloaded and is availble in $PSScriptRoot\$NameUpper
+
+Press enter to close this window"
+exit
