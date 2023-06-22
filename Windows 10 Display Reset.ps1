0f91b74bcf884a3b942837f481bc5b83690e9039
Randy2727 <19766404+Randy2727@users.noreply.github.com>
2023-06-21 22:10:24 -0700

    Create Windows 10 Display Reset.ps1

---
+++ Windows 10 Display Reset.ps1
@@ -0,0 +1,7 @@
+﻿#Removes Windows 10 remembered display settings.
+
+Remove-Item -Path HKLM:\SYSTEM\CurrentControlSet\Control\GraphicsDrivers\Configuration -Force
+ 
+Remove-Item -Path HKLM\SYSTEM\CurrentControlSet\Control\GraphicsDrivers\Connectivity -Force
+
+Remove-Item -Path HKLM:\SYSTEM\CurrentControlSet\Control\GraphicsDrivers\ScaleFactors -Force
